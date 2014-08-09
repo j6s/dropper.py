434696f5f50701ae3e24401047821eba95f7843c
Johannes Hertenstein <johannes@nimius.net>
2014-08-09 15:45:49 +0200

    initial commit

---
+++ dropper.py
@@ -0,0 +1,153 @@
+__author__ = 'thephpjo'
+import dropbox
+import webbrowser
+import json
+import os
+import logging
+import ntpath
+
+
+remote_path = "/"
+local_path = "./dropbox_data/"
+
+
+# ################################################
+
+class Dropbox:
+    def __init__(self, access_token):
+        self.dropbox = dropbox.client.DropboxClient(access_token)
+
+    def getMeta(self, path):
+        return self.dropbox.metadata(path)
+
+    def download(self, path, local_path):
+        f, metadata = self.dropbox.get_file_and_metadata(path)
+
+        with open(local_path, "wb") as out:
+            out.write(f.read())
+        return metadata
+
+
+class Folder:
+    def __init__(self, dropbox, path, local_prefix):
+        path = path.lower();
+        print(path)
+        self.local_path = local_prefix + path + "/"
+
+        self.path = path
+        self._meta = dropbox.getMeta(path)
+        # print(self._meta)
+
+        self.create_local()
+        self.meta = self.get_meta()
+
+        self.children = []
+        for item in self._meta["contents"]:
+            new_path = item["path"]
+            if item["is_dir"]:
+                print("")
+                self.children.append(Folder(dropbox, new_path, local_prefix))
+            else:
+                self.children.append(File(dropbox, new_path, local_prefix, self.meta))
+
+    def get_meta(self):
+        if not os.path.exists(self.local_path + ".dropper_data.json"):
+            with open(self.local_path + ".dropper_data.json", "w") as out:
+                out.write("{}")
+
+        json_data = open(self.local_path + ".dropper_data.json")
+        meta = json.load(json_data)
+        json_data.close()
+        # print(meta)
+        return meta
+
+
+    def create_local(self):
+        if not os.path.exists(self.local_path):
+            os.makedirs(self.local_path)
+
+
+class File:
+    def __init__(self, dropbox, path, local_prefix, meta):
+        path = path.lower();
+        self.folder_meta = meta
+        self.filename = ntpath.basename(path)
+        self.dropbox = dropbox
+        print(path)
+        self.local_path = local_prefix + path
+
+        self.path = path;
+        self._meta = dropbox.getMeta(path);
+
+        self.download_if_needed();
+
+    def needs_download(self):
+        try:
+            # checks if the revision-numbers differ
+            if self._meta["revision"] != self.folder_meta[self.filename]:
+                return True
+            else:
+                return False
+        except KeyError:
+            # the KeyError gets thrown, if a Key in the .dropper_data.json does not exist
+            # that happens, if the file is new on the server, so it has to be downloaded as well
+            return True
+
+    def download_if_needed(self):
+        if self.needs_download():
+            print("needs download")
+            new_meta = self.dropbox.download(self.path, self.local_path);
+            self.write_meta(new_meta)
+        else:
+            print("no need for downloading")
+
+    def write_meta(self, new_meta):
+        tmp = {}
+        p = ntpath.dirname(self.local_path) + "/.dropper_data.json"
+
+        with open(p, "r") as out:
+            tmp = json.load(out)
+
+        tmp[self.filename] = new_meta["revision"]
+
+        with open(p, "w") as out:
+            json.dump(tmp, out)
+
+
+######################################################
+
+
+def do_authorization():
+    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(config["app_key"], config["app_secret"])
+    authorize_url = flow.start()
+    webbrowser.open_new(authorize_url)
+    print(authorize_url)
+
+    code = input("auth code: ")
+    config["access_token"], config["user_id"] = flow.finish(code)
+
+    with open(".dropper.json", "w") as out:
+        json.dump(config, out)
+
+
+def authorize():
+    load_configuration()
+    logging.info(config);
+    if not config["access_token"] or not config["user_id"]:
+        do_authorization();
+
+
+def load_configuration():
+    global config
+
+    json_data = open(".dropper.json")
+    config = json.load(json_data)
+    json_data.close()
+
+
+####################################################
+
+authorize()
+drop = Dropbox(config["access_token"])
+
+root_folder = Folder(drop, remote_path, local_path);
