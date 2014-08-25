92061e084868b624175f8cc4755114f09acdbbc4
Johannes Hertenstein <johannes@nimius.net>
2014-08-25 09:25:35 +0200

    New cli interface, see USAGE
    - Also, USAGE is outputted, everytime the syntax is wrong

--- dropper.py
+++ dropper.py
@@ -1,12 +1,41 @@
 __author__ = 'thephpjo'
 
 from src.Dropper import Dropper
+import sys
+import itertools
+import os
+
+# Helperfunction to output the usage information
+def print_usage():
+    print("\n\n")
+    with open("USAGE") as out:
+        print(out.read())
+    print("\n\n")
 
 
-remote_path = "/01_EISBAEREN/"
-local_path = "./dropbox_data/01_EISBAEREN/"
+# Helperfunction for errors
+def error(msg):
+    print("Error: {0}".format(msg))
+    print_usage()
+
+# get the arguments: The first one is the action
+action = sys.argv[1]
+args = []
+for arg in itertools.islice(sys.argv,2,len(sys.argv)):
+    args.append(arg)
 
 
 
-drop = Dropper(remote_path,local_path)
-drop.download_only()
+# switch depending on the usage
+if  "download_only" in action or "dlo" in action:
+    if len(args) < 2:
+        error("download_only needs 2 arguments, {0} given".format(len(args)))
+    else:
+        drop = Dropper(args[0],args[1])
+        drop.download_only()
+else:
+    error("Action {0} not found".format(action))
+
+
+
+
