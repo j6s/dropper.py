b134f28ffc3355972050ca4755bbf8240c8f7d94
Johannes Hertenstein <johannes@nimius.net>
2014-08-26 20:07:08 +0200

    Show usage if no action is specified

--- dropper.py
+++ dropper.py
@@ -15,27 +15,26 @@ def print_usage():
 
 # Helperfunction for errors
 def error(msg):
-    print("Error: {0}".format(msg))
+    print("\nError: {0}".format(msg))
     print_usage()
 
 # get the arguments: The first one is the action
-action = sys.argv[1]
-args = []
-for arg in itertools.islice(sys.argv,2,len(sys.argv)):
-    args.append(arg)
+try:
+    action = sys.argv[1]
+    args = []
+    for arg in itertools.islice(sys.argv,2,len(sys.argv)):
+        args.append(arg)
 
 
 
-# switch depending on the usage
-if  "download_only" in action or "dlo" in action:
-    if len(args) < 2:
-        error("download_only needs 2 arguments, {0} given".format(len(args)))
+    # switch depending on the usage
+    if  "download_only" in action or "dlo" in action:
+        if len(args) < 2:
+            error("download_only needs 2 arguments, {0} given".format(len(args)))
+        else:
+            drop = Dropper(args[0],args[1])
+            drop.download_only()
     else:
-        drop = Dropper(args[0],args[1])
-        drop.download_only()
-else:
-    error("Action {0} not found".format(action))
-
-
-
-
+        error("Action {0} not found".format(action))
+except IndexError:
+    error("Action or Argument missing")
